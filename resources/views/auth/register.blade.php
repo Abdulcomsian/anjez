4b44fdef1dab9027db34b4fc119c8f5632a07daf
Haroon <haroonniaz96github@gmail.com>
2023-08-29 16:08:54 +0500

    front error displayed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,7 +37,17 @@
                         </span>
                     </div>
                 </div>
+                
                 <div class="form-container">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     <form method="POST" id="signup-form" action="{{ route('user.signup') }}">
                         @csrf
                         <div class="nav-div">
