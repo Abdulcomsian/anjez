81e64956c3814a3abbaa0c863418e4e26fe1bfbb
muhammad ehtisham <muhammad.ehtisham@accrualhub.com>
2023-08-30 16:07:37 +0500

    both sidebar working , extra links remove

--- resources/views/frontend/studentdashboard/layouts/course/sidebar.blade.php
+++ resources/views/frontend/studentdashboard/layouts/course/sidebar.blade.php
@@ -163,7 +163,7 @@ use App\Helpers\Helper;
 
 </div> --}}
 {{-- {{ view('frontend.studentdashboard.layouts.course.lessons') }} --}}
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
     <script src="{{ asset('assets/js/quiz.js') }}"></script>
     {{-- <script src="./assets/js/student-content.js"></script>
     <script src="./assets/js/placeholder.js"></script> --}}
