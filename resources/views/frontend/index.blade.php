64c84fb32fc6e04278cd89d9e4bd8c13f170103a
Haroon <haroonniaz96github@gmail.com>
2023-08-30 18:06:14 +0500

    resolved

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -9,7 +9,7 @@ use App\Helpers\Helper;
   <div>
   <nav class="navbar navbar-expand-lg navbar-light">
       <div class="container">
-        <a class="navbar-brand" href="/"><img src="{{ url('assets/images/Group 6.png') }}" /></a>
+        <a class="navbar-brand" href="{{route("frontend")}}"><img src="{{ url('assets/images/Group 6.png') }}" /></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
